0e95ec74c1ed7a12d3a705f8223acb9909dae0d2
Niket Shukla <47295160+niketshukla@users.noreply.github.com>
2020-08-15 12:55:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+//using selectors inside the element
+const questions = document.querySelectorAll(".question");
+console.log(questions);
+questions.forEach(function (accord) {
+    const btn = accord.querySelector(".question-btn");
+    console.log(btn);
+
+    btn.addEventListener("click", function () {
+
+    questions.forEach(function (item) {
+        console.log(item);
+        if (item !== accord) {
+        item.classList.remove("show-text");
+        }
+    });
+
+    accord.classList.toggle("show-text");
+  });
+});
